c91f546fef57406ef1ce0aed3fa0e746e578ebcf
maike1983 <Maikevandartel@outlook.com>
2026-05-06 12:50:08 +0200

    Stap 2 formulier afgerond

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ function App() {
     const [bananas, setBananas] = React.useState(0);
     const [apples, setApples] = React.useState(0);
     const [kiwis, setKiwis] = React.useState(0);
+    const [firstNameValue, setFirstNameValue] = React.useState("");
+    const [lastNameValue, setLastNameValue] = React.useState("");
+    const [ageValue, setAgeValue] = React.useState(0);
+    const [zipCodeValue, setZipCodeValue] = React.useState("");
+    const [deliveryFrequencyValue, setDeliveryFrequencyValue] = React.useState("every-week");
+    const [deliveryMomentValue, setDeliveryMomentValue] = React.useState("day");
+    const [remarksValue, setRemarksValue] = React.useState("");
+    const [termsAndConditionsValue, setTermsAndConditionsValue] = React.useState(false);
 
     function resetAll() {
         setStrawberries(0);
@@ -14,6 +22,24 @@ function App() {
         setApples(0);
         setKiwis(0);
     }
+
+    function handleSubmitButtonClick() {
+        console.log({
+            strawberries,
+            bananas,
+            apples,
+            kiwis,
+            firstNameValue,
+            lastNameValue,
+            ageValue,
+            zipCodeValue,
+            deliveryFrequencyValue,
+            deliveryMomentValue,
+            remarksValue,
+            termsAndConditionsValue,
+        });
+    }
+
     return (
         <>
             <h1>Fruitmand bezorgservice</h1>
@@ -46,6 +72,101 @@ function App() {
                 </ul>
                 <button type="button" onClick={resetAll}>Reset</button>
             </section>
+            <form>
+                <label htmlFor="first-name">
+                    Voornaam
+                    <input
+                        type="text"
+                        id="first-name"
+                        name="first-name"
+                        value={firstNameValue}
+                        onChange={(e) => setFirstNameValue(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="last-name">
+                    Achternaam
+                    <input
+                        type="text"
+                        id="last-name"
+                        name="last-name"
+                        value={lastNameValue}
+                        onChange={(e) => setLastNameValue(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="age">
+                    Leeftijd
+                    <input
+                        type="number"
+                        id="age"
+                        name="age"
+                        value={ageValue}
+                        placeholder="0"
+                        onChange={(e) => setAgeValue(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="zip-code">
+                    Postcode
+                    <input
+                        type="text"
+                        id="zip-code"
+                        name="zip-code"
+                        value={zipCodeValue}
+                        onChange={(e) => setZipCodeValue(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="delivery-frequency">Bezorgfrequentie</label>
+                <select name="delivery-frequency"
+                        id="delivery-frequency"
+                        value={deliveryFrequencyValue}
+                        onChange={(e) => setDeliveryFrequencyValue(e.target.value)}
+                >
+                    <option value="every-week">Iedere week</option>
+                    <option value="every-other-week">Om de week</option>
+                    <option value="every-month">Iedere maand</option>
+                </select>
+                <fieldset>
+                    <input
+                        type="radio"
+                        id="day-delivery"
+                        name="deliveryMoment"
+                        value="day"
+                        checked={deliveryMomentValue === "day"}
+                        onChange={(e) => setDeliveryMomentValue(e.target.value)}
+                    />
+                    <label htmlFor="day-delivery">Overdag</label>
+                    <input
+                        type="radio"
+                        id="evening-delivery"
+                        name="deliveryMoment"
+                        value="evening"
+                        checked={deliveryMomentValue === "evening"}
+                        onChange={(e) => setDeliveryMomentValue(e.target.value)}
+                    />
+                    <label htmlFor="evening-delivery">'s Avonds</label>
+                </fieldset>
+                <label htmlFor="remarks">Opmerking</label>
+                <textarea
+                    id="remarks"
+                    name="remarks"
+                    rows="4"
+                    cols="50"
+                    value={remarksValue}
+                    onChange={(e) => setRemarksValue(e.target.value)}>
+                </textarea>
+                <label htmlFor="form-terms-and-conditions">
+                    <input
+                        type="checkbox"
+                        id="form-terms-and-conditions"
+                        name="terms-and-conditions"
+                        checked={termsAndConditionsValue}
+                        onChange={(e) => setTermsAndConditionsValue(e.target.checked)}
+                    />
+                    Ik ga akkoord met de voorwaarden
+                </label>
+                <button type="button" onClick={handleSubmitButtonClick}>
+                    Verzenden
+                </button>
+            </form>
         </>
     )
 }
